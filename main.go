b1e9712b46b2f23ea2624d05682534788a96c341
nandonyata <nandonp02@gmail.com>
2024-01-22 20:43:11 +0700

    feat: applyin proto

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"grpc_go/pb"
+	"log"
+
+	"google.golang.org/protobuf/proto"
+)
 
 func main() {
-	fmt.Println("WOIii...")
+
+	products := pb.Products{
+		Pagination: &pb.Pagination{
+			Total:       2,
+			PerPage:     2,
+			CurrentPage: 1,
+			LastPage:    1,
+		},
+		Data: []*pb.Product{
+			{
+				Id:    1,
+				Name:  "POLO SHIRT",
+				Price: 70000,
+				Stock: 3,
+				Category: &pb.Category{
+					Id:   1,
+					Name: "Shirt",
+				},
+			},
+			{
+				Id:    1,
+				Name:  "LEVIS SHIRT",
+				Price: 130000,
+				Stock: 7,
+				Category: &pb.Category{
+					Id:   1,
+					Name: "Shirt",
+				},
+			},
+		},
+	}
+
+	data, err := proto.Marshal(&products)
+	if err != nil {
+		log.Fatal("Err marshal: ", err)
+	}
+
+	fmt.Println("Data after marshal: ", data)
+
+	var dataAfter pb.Products
+
+	err = proto.Unmarshal(data, &dataAfter)
+	if err != nil {
+		log.Fatal("Err unmarshal: ", err)
+	}
+
+	fmt.Println("Data after unmarshal: ", dataAfter)
 }
